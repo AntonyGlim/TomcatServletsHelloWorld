abf1660e447c2df805b30757b4f336f556ea3731
Antony.Glim@gmail.com <Antony.Glim@gmail.com>
2019-08-01 18:30:05 +0300

    Life ccycle

--- ee/src/HelloWorld.java
+++ ee/src/HelloWorld.java
@@ -1,4 +1,6 @@
 import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -7,8 +9,34 @@ import java.io.IOException;
 
 @WebServlet("/hw")
 public class HelloWorld extends HttpServlet {
+
+    /*Выполняется единожды при создании сервлета*/
+    @Override
+    public void init() throws ServletException {
+        System.out.println("init Method");
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().write("Hello World!!!!!!!!!!!");
+        System.out.println("doGet Method");
+        resp.getWriter().write("Hello World!!!!!!!!!!!+++++++++++");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+
+    /*метод обрабатывает все наши запросы*/
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        System.out.println("service Method");
+        super.service(req, res);
+    }
+
+    /*не стоит на него полагаться*/
+    @Override
+    public void destroy() {
+        System.out.println("destroy Method");
     }
 }
