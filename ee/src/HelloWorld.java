864ac79dda0bb9b468ab17bbe23847e5f8ef0908
Antony.Glim@gmail.com <Antony.Glim@gmail.com>
2019-08-02 19:13:59 +0300

    parameters on page

--- ee/src/HelloWorld.java
+++ ee/src/HelloWorld.java
@@ -25,38 +25,61 @@ public class HelloWorld extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("doGet Method");
-        System.out.println("Thread: " + Thread.currentThread().getName());
-        resp.getWriter().write("Hello World!!!!!!!!!!!+++++++++++");
-        synchronized (this){
-            /*Демонстрация потокоопасности*/
-            for (int j = 0; j < 1_000_000; j++) {
-                i++;
-            }
-            System.out.println(i);
-        }
-        //req - отвечает за все что приходит от клиента
-        System.out.println();
-        System.out.println("Работа с параметрами");
-        //Запрос будет иметь вид: http://localhost:8081/hw?name=First&one=1&two=2&two=22&two=222&two=2222&two=22222
-        System.out.println(req.getParameter("name"));
-        System.out.println(req.getParameter("one"));
-        String[] twos = req.getParameterValues("two");
-        for (String two : twos) {
-            System.out.println("value: " + two);
-        }
-        Enumeration<String> parameterNames = req.getParameterNames();//имена всех параметров
-        while (parameterNames.hasMoreElements()){
-            System.out.println("name: " + parameterNames.nextElement());
-        }
-        Map<String, String[]> parameterMap = req.getParameterMap();
-        System.out.println(req.getRequestURL());
-        System.out.println(req.getRequestURI());
-        System.out.println(req.getServletPath());
-        System.out.println(req.getRemoteHost());
-        System.out.println(req.getLocalPort());
+//        System.out.println("doGet Method");
+//        System.out.println("Thread: " + Thread.currentThread().getName());
+////        resp.getWriter().write("Hello World!!!!!!!!!!!+++++++++++");
+//        synchronized (this){
+//            /*Демонстрация потокоопасности*/
+//            for (int j = 0; j < 1_000_000; j++) {
+//                i++;
+//            }
+//            System.out.println(i);
+//        }
+//        //req - отвечает за все что приходит от клиента
+//        System.out.println();
+//        System.out.println("Работа с параметрами");
+//        //Запрос будет иметь вид: http://localhost:8081/hw?name=First&one=1&two=2&two=22&two=222&two=2222&two=22222
+//        System.out.println(req.getParameter("name"));
+//        System.out.println(req.getParameter("one"));
+//        String[] twos = req.getParameterValues("two");
+//        for (String two : twos) {
+//            System.out.println("value: " + two);
+//        }
+//        Enumeration<String> parameterNames = req.getParameterNames();//имена всех параметров
+//        while (parameterNames.hasMoreElements()){
+//            System.out.println("name: " + parameterNames.nextElement());
+//        }
+//        Map<String, String[]> parameterMap = req.getParameterMap();
+//        System.out.println(req.getRequestURL());
+//        System.out.println(req.getRequestURI());
+//        System.out.println(req.getServletPath());
+//        System.out.println(req.getRemoteHost());
+//        System.out.println(req.getLocalPort());
+//
+//        System.out.println(req.getQueryString()); //name=First&one=1&two=2&two=22&two=222&two=2222&two=22222
 
-        System.out.println(req.getQueryString()); //name=First&one=1&two=2&two=22&two=222&two=2222&two=22222
+        String firstname = req.getParameter("firstname");
+        String lastname = req.getParameter("lastname");
+        resp.getWriter().write("<!DOCTYPE html>\n" +
+                "<html lang=\"ru\">\n" +
+                "<head>\n" +
+                "\t<title>Контакты</title>\n" +
+                "\t<meta charset=\"utf-8\">\n" +
+                "\t<link href=\"styles/styles.css\" rel=\"stylesheet\">\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "\t<h1>Hello World!!!!!!!!!!!+++++++++++</h1>\n" +
+                "firstname= " + firstname + "<br>" +
+                "lastname= " + lastname + "<br>" +
+                "\t<form action=\"hw\" method=\"get\">\n" +
+                "\t\tFirst name:<br>\n" +
+                "\t\t<input type=\"text\" name=\"firstname\"><br>\n" +
+                "\t\tLast name:<br>\n" +
+                "\t\t<input type=\"text\" name=\"lastname\"><br>\n" +
+                "\t\t<input type=\"submit\" name=\"submit\">\n" +
+                "\t</form>\n" +
+                "</body>\n" +
+                "</html>");
     }
 
     @Override
